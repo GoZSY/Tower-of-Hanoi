eb0f4b2893a7c80055b0347800af0ad697cae0ab
GoZSY <13207508051@163.com>
2016-06-27 22:17:12 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -23,3 +23,65 @@ void func(int n, string from, string mid, string to)
         func(n-1, mid, from, to);
     }
 }
+//进阶参考解答
+/**********************************************************
+时间复杂度O(N),空间复杂度O(N)
+***********************************************************/
+int process(vector<int> arr, int i, int from, int mid, int to)
+{
+    if(i == -1)
+        return 0;
+    if(arr[i] != from && arr[i] != to)
+        return 0;
+    if(arr[i] == from)
+        return process(arr, i-1,from, to, mid);
+    if(arr[i] == to)
+    {
+        int res = process(arr, i-1,mid,from,to);
+        if(res == -1)
+            return -1;
+        return res + (1 << i);
+    }
+}
+int step1(vector<int> arr)
+{
+    if(arr.size()==0)
+        return -1;
+    return process(arr, arr.size()-1, 1, 2, 3);
+}
+
+/**********************************************************
+时间复杂度O(N),空间复杂度O(1)
+***********************************************************/
+
+int step2(vector<int> arr)
+{
+    if(arr.size()==0)
+        return -1;
+    int from = 1;
+    int mid = 2;
+    int to = 3;
+    int i = arr.size() - 1;
+    int res = 0;
+    int temp = 0;
+    while(i >= 0)
+    {
+        if(arr[i] != from && arr[i] != to)
+            return -1;
+        if(arr[i] == from)
+        {
+            temp = to;
+            to = mid;
+        }
+        if(arr[i] == to)
+        {
+            res += 1<<i;
+            temp = from;
+            from = mid;
+            
+        }
+        mid = temp;
+        i--;
+    }
+    return res;
+}
